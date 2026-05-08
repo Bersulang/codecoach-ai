c77f62c727f41d190b49807819bbf7a8003b3567
Bersulang <bersulang0627@gmail.com>
2026-05-09 02:31:36 +0800

    feat: add interview message entity and mapper

---
+++ backend/src/main/java/com/codecoach/module/interview/mapper/InterviewMessageMapper.java
@@ -0,0 +1,7 @@
+package com.codecoach.module.interview.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.codecoach.module.interview.entity.InterviewMessage;
+
+public interface InterviewMessageMapper extends BaseMapper<InterviewMessage> {
+}
