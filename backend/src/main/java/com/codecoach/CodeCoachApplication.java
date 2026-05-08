f6e8396b8c0613833246816e038674ee5a654404
Bersulang <bersulang0627@gmail.com>
2026-05-08 20:14:36 +0800

    feat: initialize spring boot backend

---
+++ backend/src/main/java/com/codecoach/CodeCoachApplication.java
@@ -0,0 +1,12 @@
+package com.codecoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCoachApplication.class, args);
+    }
+}
